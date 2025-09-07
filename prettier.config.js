e6ebf37abaa0f7682a1553c2341642ac3667eb34
younusmyoffice <younusmyoffice@gmail.com>
2025-09-07 23:42:23 +0530

    new version 1.0.0 prd ready code stable

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    printWidth: 100,
-    singleQuote: false,
-    trailingComma: "all",
-    bracketSpacing: true,
-    tabWidth: 4,
-    semi: true,
-};
--- src/signup/HCFsignup/AdminSignup/AdminSignup.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminSignUp = () => {
-    return <>AdminSignUp</>;
-};
-
-export default AdminSignUp;
